2f06e16b8eeb3ea819303d10c833c35f0745455f
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-10-24 15:43:07 -0500

    Add functionality to add clicked numebr to displayed number

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@ document.querySelector('.calculator-btns').addEventListener('click', function(ev
   const display = document.querySelector('.calculator-display');
 
   //checks that clicked button is a number
-  if (event.target.classList.contains('numb'))
-    //
-    if (display.innerText !== 0) {
+  if (event.target.classList.contains('numb')) {
+    //checks that display is 0 before overriding inner text
+    if (display.innerText === "0") {
       display.innerText = event.target.innerText;
+    //otherwise adds clicked number to display
+    } else {
+      display.innerText += event.target.innerText;
     }
-})
+  }
+
+});
