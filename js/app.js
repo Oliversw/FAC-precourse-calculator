424bd2aa381751907de14e4a887b210a6ea3b25c
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-10-25 17:33:32 -0500

    Refactor code to use variable for button clicked

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
+//listens for button clicks
 document.querySelector('.calculator-btns').addEventListener('click', function(event) {
   const display = document.querySelector('.calculator-display');
+  let buttonClicked = event.target.innerText;
 
   //checks that clicked button is a number
   if (event.target.classList.contains('numb')) {
     //checks that display is 0 before overriding inner text
     if (display.innerText === "0") {
-      display.innerText = event.target.innerText;
+      display.innerText = buttonClicked;
     //otherwise adds clicked number to display
     } else {
-      display.innerText += event.target.innerText;
+      display.innerText += buttonClicked;
     }
   }
 
