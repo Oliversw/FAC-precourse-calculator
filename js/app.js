b0b6edb0a1a5d41da825fafb7d5c939309c44ad8
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-10-25 18:14:59 -0500

    Add basic functionality to click each remaining button

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 document.querySelector('.calculator-btns').addEventListener('click', function(event) {
   const display = document.querySelector('.calculator-display');
   let buttonClicked = event.target.innerText;
+  let buttonClasses = event.target.classList;
 
   //checks that clicked button is a number
-  if (event.target.classList.contains('numb')) {
+  if (buttonClasses.contains('numb')) {
     //checks that display is 0 before overriding inner text
     if (display.innerText === "0") {
       display.innerText = buttonClicked;
@@ -14,4 +15,37 @@ document.querySelector('.calculator-btns').addEventListener('click', function(ev
     }
   }
 
+  //checks whether clicked button is an operator (+ - * / =)
+  if (buttonClasses.contains('operator')) {
+    console.log("operator");
+    if (buttonClasses.contains('plus')) {
+      console.log("plus");
+    }
+    if (buttonClasses.contains('min')) {
+      console.log("min");
+    }
+    if (buttonClasses.contains('mult')) {
+      console.log("mult");
+    }
+    if (buttonClasses.contains('div')) {
+      console.log("div");
+    }
+    if (buttonClasses.contains('equals')) {
+      console.log("equals");
+    }
+  }
+
+  //checks whether clicked button is cancel
+  if (buttonClasses.contains('dark')) {
+    console.log("dark");
+    if (buttonClasses.contains('cancel')) {
+      console.log("AC");
+    }
+    if (buttonClasses.contains('plusneg')) {
+      console.log("plusneg");
+    }
+    if (buttonClasses.contains('pcnt')) {
+      console.log("pcnt");
+    }
+  }
 });
